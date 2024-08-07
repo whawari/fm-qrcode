c476a3490d768469351794ca65ebc473463e68ad
Walid Hawari <walidhawari95@gmail.com>
2024-08-07 09:10:40 +0300

    [UPDATE]: finished qr code component development

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        paragraph: "15px",
+      },
+    },
     fontFamily: {
       outfit: ["Outfit", "sans-serif"],
+    },
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      slate: {
+        300: "hsl(212, 45%, 89%)",
+        500: "hsl(216, 15%, 48%)",
+        900: "hsl(218, 44%, 22%)",
+      },
     },
   },
   plugins: [],
